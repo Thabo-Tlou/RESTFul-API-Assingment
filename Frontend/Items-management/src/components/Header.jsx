8ce9fcac4ca7d65fd01c1c19c456cc37a0edd2fe
thatochelane <thatochelane9@gmail.com>
2024-12-11 03:36:37 +0200

    components

---
+++ Frontend/Items-management/src/components/Header.jsx
@@ -0,0 +1,24 @@
+// Header.jsx
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Header.css'; // Import CSS for Header
+
+const Header = () => {
+    return ( <
+        header className = "header" >
+        <
+        div className = "header__content" >
+        <
+        h > Items Stock Management System < /h> <
+        nav className = "header__nav" >
+        <
+        ul className = "header__nav-list" >
+        <
+        /ul> < /
+        nav > <
+        /div> < /
+        header >
+    );
+};
+
+export default Header;
